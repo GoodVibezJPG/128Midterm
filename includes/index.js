87ee38dba2f769723eb769f87c46f287593871ff
Caleb Irvine <irvinecaleb1@gmail.com>
2025-03-10 11:19:35 -0700

    fix for loop

--- includes/index.js
+++ includes/index.js
@@ -118,14 +118,14 @@ function userView (userFound) {
     } else{
         //Else only show the user and admins
         for (let i = 0; i < users.length; i++) {
-            let admin = users[i]
-            if (admin.roleIndicator === true) {
+            let user = users[i]
+            if (user.roleIndicator === true) {
             masonry.innerHTML += `
                 <div class="col-sm-6 col-lg-4 mb-4">
                         <div class="card p-3">
-                            <div class="circle mx-auto"><img src="includes/assets/${admin.visualId}.jpg" class="card-img-top" alt="a picture of ${admin.fName} ${admin.lName})"></div>
+                            <div class="circle mx-auto"><img src="includes/assets/${user.visualId}.jpg" class="card-img-top" alt="a picture of ${user.fName} ${user.lName})"></div>
                             <div class="card-body">
-                                <h5 class="card-title text-center">${admin.fName} ${admin.lName}</h5>
+                                <h5 class="card-title text-center">${user.fName} ${user.lName}</h5>
                                 <p class="card-text">user.description</p>
                                 <a href="#" class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas">>Go somewhere</a>
                             </div>
